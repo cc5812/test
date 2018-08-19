6f601b0eeef841fe742bf6a0c70a8e22bd234de2
cc5812 <1726835215@qq.com>
2018-08-19 19:09:16 +0800

    Revert "ceshi"
    This reverts commit e28a1d895af3a760295531e003df7b63f0dfc9e9.

--- src/test2.java
+++
@@ -1,9 +0,0 @@
-
-public class test2 {
-	public static void main(String[] args) {
-		String aa = "aaahellbbb";
-		String bb = "";
-		bb = aa.replace("hellbbb", "wwwccc");
-		System.out.println(bb);
-}
-}
